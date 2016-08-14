c9318735046e1dbc4131ae79b5e3d2d01cf496b5
Samuel Zuniga Vanoye <spankly@neuronet.com.mx>
2016-08-13 22:39:10 -0500

    Add reduce moviees sample, to illustrate flat mapping

---
+++ src/reduce-movies.js
@@ -0,0 +1,59 @@
+let movies = [
+  {
+    title: "Batman Begins",
+    year: 2005,
+    cast: [
+      "Christian Bale",
+      "Michael Caine",
+      "Liam Neeson",
+      "Katie Holmes",
+      "Gary Oldman",
+      "Cillian Murphy"
+    ]
+  },
+  {
+    title: "The Dark Knight",
+    year: 2008,
+    cast: [
+      "Christian Bale",
+      "Heath Ledger",
+      "Aaron Eckhart",
+      "Michael Caine",
+      "Maggie Gyllenhal",
+      "Gary Oldman",
+      "Morgan Freeman"
+    ]
+  },
+  {
+    title: "The Dark Knight Rises",
+    year: 2012,
+    cast: [
+      "Christian Bale",
+      "Gary Oldman",
+      "Tom Hardy",
+      "Joseph Gordon-Levitt",
+      "Anne Hathaway",
+      "Marion Cotillard",
+      "Morgan Freeman",
+      "Michael Caine"
+    ]
+  }
+];
+
+let reducer = (acc, movie) => {
+    movie.cast.forEach((actor)=> {
+	if(acc.indexOf(actor) == -1) {
+	    acc.push(actor);
+	}
+    });
+
+    return acc;
+}
+
+let castOriginal = movies.reduce(reducer,[]);
+let cast = castOriginal.slice(0);
+
+console.log('The cast is', cast.sort());
+console.log(castOriginal);
+
+
